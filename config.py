9b9427db6cf3f645dab9c13339ab6b44d54f558d
Vickyy <148709830+vivekbhavsar59@users.noreply.github.com>
2025-06-30 22:47:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 123))
+    API_ID = int(os.environ.get("API_ID", ))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
